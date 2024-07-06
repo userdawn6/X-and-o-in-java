4327ef21b1c3b22a7b91342917232fafb8218874
Ayomide Oyewole <129783781+Ayomide0811@users.noreply.github.com>
2024-07-06 11:11:04 +0100

    .

--- XandO.java
+++ XandO.java
@@ -22,3 +22,37 @@ public class XandO {
             {0, 4, 8},
             {2, 4, 6}
     };
+public XandO() {
+        drawUI();
+    }
+
+    void drawUI() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 650);
+        frame.setLayout(new BorderLayout());
+        frame.setLocationRelativeTo(null);
+
+        textlabel.setBackground(Color.black);
+        textlabel.setForeground(Color.gray);
+        textlabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textlabel.setHorizontalAlignment(JLabel.CENTER);
+        textlabel.setText("X and O");
+        textlabel.setOpaque(true);
+
+        textpanel.setLayout(new BorderLayout());
+        textpanel.add(textlabel, BorderLayout.CENTER);
+        frame.add(textpanel, BorderLayout.NORTH);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(3, 3));
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = new JButton("");
+            buttons[i].setFont(new Font("Arial", Font.BOLD, 100));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(new ButtonClickListener());
+            buttonPanel.add(buttons[i]);
+        }
+        frame.add(buttonPanel);
+
+        frame.setVisible(true);
+    }
